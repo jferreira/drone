475ec94bd920cf521d0947d91c6f1066cd087e8d
Dharam Whabi <dharam_w@hotmail.com>
2018-11-08 15:12:33 +0800

    Update routes.rb
    @jferreira deleted auto generated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,4 @@
 Rails.application.routes.draw do
-  get 'packages/index'
-  get 'packages/show'
-  get 'packages/new'
-  get 'packages/edit'
-  get 'users/index'
-  get 'users/new'
-  get 'users/edit'
-  get 'users/show'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
