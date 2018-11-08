faba0353e713c0e54239dda03c248b10b6c274ac
Dharam Whabi <dharam_w@hotmail.com>
2018-11-08 15:15:49 +0800

    Update bookings_controller.rb
    @jferreira updated the controller file with all the CRUD functions

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,12 +2,21 @@ class BookingsController < ApplicationController
   def index
   end
 
-  def show
-  end
-
   def new
   end
 
+  def create
+  end
+  
+  def show
+  end
+  
   def edit
+  end
+  
+  def update
+  end
+  
+  def destroy
   end
 end
