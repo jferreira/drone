99dbe878d5a507112ecf46e728b7c04de1ee0032
Roger Garcia <r1ger_pg@hotmail.com>
2018-11-08 13:13:27 +0800

    Add basic validation for bookings

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,11 @@
 class Booking < ApplicationRecord
   belongs_to :package
   belongs_to :user
+
+  validates :title, :location, :start_datetime, :end_datetime, :cost, :package_id, presence: true
+  validates :status, :presence: true, default: 'Pending', inclusion: { in: w%[Pending Accepted Rejected] }
+
+  # def time_is_now?(date)
+  #   DateTime.now > date
+  # end
 end
