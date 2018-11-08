e30e45e01d87e2a7dbd4f78f9c211a12f41d3ab4
Roger Garcia <r1ger_pg@hotmail.com>
2018-11-08 17:00:47 +0800

    Seeding scripts done. IMP: changes to model validations required and done in separate commit

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,7 +3,7 @@ class Booking < ApplicationRecord
   belongs_to :user
 
   validates :title, :location, :start_datetime, :end_datetime, :cost, :package_id, presence: true
-  validates :status, :presence: true, default: 'Pending', inclusion: { in: w%[Pending Accepted Rejected] }
+  validates :status, presence: true, inclusion: { in:   %w[Pending Accepted Rejected] }
 
   # def time_is_now?(date)
   #   DateTime.now > date
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,19 @@
 
 
+#### User Creation ####
 
-arr = %w[a b c d e f]
-
+letters = %w[q w e r t y]
 puts 'Start creating user accounts'
 
-arr.each do |x|
+letters.each do |x|
 
-puts "Account with '#{x}' created"
+puts "User account with '#{x}' created"
 
   user_data = {
     first_name: "#{x}",
     last_name: "#{x}",
     email: "#{x}@#{x}.com",
-    password: "#{x}"
+    password: "#{x}#{x}#{x}#{x}#{x}#{x}"
   }
 
   user = User.new(user_data)
@@ -22,6 +22,66 @@ puts "Account with '#{x}' created"
 end
 
 puts 'Finished creating user accounts'
+
+
+### Package Creation ####
+
+puts 'Start creating packages data'
+
+image_url = 'https://azcd.harveynorman.com.au/media/catalog/product/cache/21/image/992x558/9df78eab33525d08d6e5fb8d27136e95/3/4/3495037_1.jpg'
+
+20.times do
+
+  puts "Package created"
+
+  package_data = {
+    title: %w[Basic Advanced Premium].sample,
+    description: 'Experience Drone guy, for 1h of filming and editing',
+    url_image: image_url,
+    price: %w[$100 $110 $120 $130 $140].sample,
+    # review_id: (1..50).to_a.sample,
+    user: User.all.sample
+  }
+
+    package = Package.new(package_data)
+    package.save!
+
+end
+
+# puts 'Finshed creating packages data'
+
+#### Booking Creation ####
+
+puts 'Start creating bookings data'
+
+30.times do
+
+  puts "Booking created"
+
+  location = %w[Barcelona Bali Paris Sydney Hyderabad Denpassar Moscow].sample
+  start_time = DateTime.now
+  end_time = start_time + 2
+
+  puts start_time
+  puts end_time
+
+  booking_data = {
+    title: "#{location} filming @ #{start_time}:00",
+    status: %w[Accepted Rejected].to_a.sample,
+    location: location,
+    start_datetime: start_time,
+    end_datetime: end_time,
+    cost: %w[$100 $110 $120 $130 $140].to_a.sample,
+    package: Package.find_by(id: (1..5).to_a.sample),
+    user: User.find_by(id: (1..5).to_a.sample)
+  }
+
+    booking = Booking.new(booking_data)
+    booking.save!
+
+end
+
+puts 'Finshed creating bookings data'
 
 
 
