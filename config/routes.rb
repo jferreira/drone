71b54b333c875a4b3998d655a4950a645b6c2035
Dharam Whabi <dharam_w@hotmail.com>
2018-11-09 16:26:16 +0800

    Edit profile updated

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'user/edit', to: 'users#edit', as:  :edit_user
 
-  resources :users, only: [:edit, :update, :show]
+  resources :users, only: [:update, :show]
   resources :packages do
     resources :bookings
   end
