2f8df54d9e6c676dd0d8f0d436feea464b9a2b17
Roger Garcia <r1ger_pg@hotmail.com>
2018-11-12 19:35:51 +0800

    Fix for bookings not being displayed

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,14 +1,13 @@
 class DashboardController < ApplicationController
 
   def my_dashboard
-    # @bookings = Booking.where(user_id: current_user.id)
-    # @my_packages = Package.where(user_id: current_user.id).pluck(:id) # [1,2,3]
+    @bookings = Booking.where(user_id: current_user.id)
+    @my_packages = Package.where(user_id: current_user.id).pluck(:id)
     # @my_jobs = Booking.where(:package_id => @my_packages)
 
-    @bookings = current_user.bookings
-    @my_packages = current_user.packages
+    # @bookings = current_user.bookings
+    # @my_packages = current_user.packages
     @my_jobs = Booking.where(:package_id => @my_packages)
-
 
   end
 
