f6ccb1ff7694aaaecfe511340fc0cb9ccb309b46
Roger Garcia <r1ger_pg@hotmail.com>
2018-11-12 15:51:50 +0800

    Add logic and views to display my_bookings and my_jobs

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,15 @@
 class DashboardController < ApplicationController
 
   def my_dashboard
-    @bookings = Booking.where(user_id: current_user.id)
+    # @bookings = Booking.where(user_id: current_user.id)
+    # @my_packages = Package.where(user_id: current_user.id).pluck(:id) # [1,2,3]
+    # @my_jobs = Booking.where(:package_id => @my_packages)
+
+    @bookings = current_user.bookings
+    @my_packages = current_user.packages
+    @my_jobs = Booking.where(:package_id => @my_packages)
+
+
   end
 
 end
