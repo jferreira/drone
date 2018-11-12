05e908335cacab3de32cfbbb712020ff3855dee8
john ferreira <john@mediasifter.co>
2018-11-12 17:00:58 +0800

    fix seed html error in content

--- db/seeds.rb
+++ db/seeds.rb
@@ -29,12 +29,11 @@ puts 'Start creating user accounts'
                   "https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?auto=compress&cs=tinysrgb&h=350",
                   "http://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjEyMDd9&s=2cbaca6f5dc24bb35aab13e97e390994",
                   "https://images.pexels.com/photos/573299/pexels-photo-573299.jpeg?auto=compress&cs=tinysrgb&h=350"],
-  package_content = ["<p>Drone Cinematography and Aerial Video Services:</p>
-                <p>We Specialize in two operator professional drone cinematography and video services, delivering stunning video impossible or prohibitively expensive to capture by traditional methods - helicopter, crane, dolly, cable-cam, or 'Russian arm'. From majestic aerial drone videos to fast, low and close-up tracking of fast moving subjects, our two operator drone teams have the expertise, experience, and passion to deliver the shots you need. We fly RED and other cinematic cameras!</p>
-                <br>
-                <p>Drone Cinematography</p>
-                <p>Our drone aerial videography services allow you to capture professional stabilized footage from a unique point-of-view in full HD quality including HD, 4K, 5.2K and 8K options. From scenic landscape flyovers to low, fast, tracking action shots, our experienced and skilled aerial cinematography team can do it all. We offer variety of equipment to work with to suit any drone video project need - from smaller action cams, through DSLR camera drones to cinematic camera drone and gimbal packages - Yes, we fly RED!</p>"]
-
+  package_content = ["Drone Cinematography and Aerial Video Services:
+                      We Specialize in two operator professional drone cinematography and video services, delivering stunning video impossible or prohibitively expensive to capture by traditional methods - helicopter, crane, dolly, cable-cam, or 'Russian arm'.
+                      From majestic aerial drone videos to fast, low and close-up tracking of fast moving subjects, our two operator drone teams have the expertise, experience, and passion to deliver the shots you need. We fly RED and other cinematic cameras!
+                      Drone Cinematography.
+                      Our drone aerial videography services allow you to capture professional stabilized footage from a unique point-of-view in full HD quality including HD, 4K, 5.2K and 8K options. From scenic landscape flyovers to low, fast, tracking action shots, our experienced and skilled aerial cinematography team can do it all. We offer variety of equipment to work with to suit any drone video project need - from smaller action cams, through DSLR camera drones to cinematic camera drone and gimbal packages - Yes, we fly RED!</p>"]
 
 25.times do |i|
   puts "------------------------------"
