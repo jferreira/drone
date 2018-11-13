2ab8ba84c875965a1ba33b82b396595bf90967be
Roger Garcia <r1ger_pg@hotmail.com>
2018-11-13 15:33:59 +0800

    Changes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,2 +1,8 @@
 //= require rails-ujs
 //= require_tree .
+
+     $("input.datepicker").each(function(input) {
+     $(this).datepicker({
+     dateFormat: "yy-mm-dd",
+     altField: $(this).next()
+      })
